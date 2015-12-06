b5b1ac7decbc066650d970e92f36ed8dc25253bc
bobans@ <bobans@inbox.ru>
2015-12-07 00:34:00 +0300

    commit

---
+++ Seach_Process/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Seach_Process
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Монитор сервиса Cbmain.ex";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(323, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Монитор сервиса Rts.exe";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(200, 7);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(53, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Интервал перепроверки сервисов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "мин.";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(408, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 54);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 116);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "[РОБОТ] Система мониторинга за процессами.";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Seach_Process/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+using System.Runtime.InteropServices;
+
+using System.IO;
+
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
+
+
+
+namespace Seach_Process
+{
+    public partial class Form1 : Form
+    {
+        public bool flag_cbank = false;
+        public bool flag_rts = false;
+
+          [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(
+                 string lpClassName,
+                 string lpWindowName
+            );
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindowEx(
+               IntPtr hWndParent,
+               IntPtr hWndChild,
+               string lpszClass,
+               string lpszWindow
+          );
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindow(
+               IntPtr hWnd,
+               int uCmd
+          );
+
+
+        [DllImport("user32.dll")]
+          public static extern int GetWindowText(
+                 IntPtr hWnd,
+                 StringBuilder lpString,
+                 int nMaxCount
+            );
+
+        [DllImport("user32.dll")]
+        public static extern int GetClassName(
+               IntPtr hWnd,
+               StringBuilder lpClassName,
+               int nMaxCount            
+          );
+        
+
+
+
+        public Form1()
+        {
+            bool flag_cbank=false;
+            bool flag_rts = false;
+
+            InitializeComponent();
+         //   this.ShowInTaskbar = false;
+        }
+
+
+        private void SendEmail(string str,int i)
+        {
+            //Авторизация на SMTP сервере
+            SmtpClient Smtp = new SmtpClient("ip", 25);
+            Smtp.Credentials = new NetworkCredential("user", "pass");
+            //Smtp.EnableSsl = false;
+
+            //Формирование письма
+            MailMessage Message = new MailMessage();
+            Message.From = new MailAddress("from");
+            Message.To.Add(new MailAddress("to"));
+
+            if (i==0) 
+            Message.Subject = "[РОБОТ] Система мониторинга за процессами. Пропал процесс";
+            else
+            Message.Subject = "[РОБОТ] Система мониторинга за процессами. Процесс восстановлен";
+            Message.Body = str;
+
+            //Прикрепляем файл
+            // string file = "C:\\file.zip";
+            //Attachment attach = new Attachment(file, MediaTypeNames.Application.Octet);
+
+            // Добавляем информацию для файла
+            //ContentDisposition disposition = attach.ContentDisposition;
+            //disposition.CreationDate = System.IO.File.GetCreationTime(file);
+            //disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
+            //disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+
+            //            Message.Attachments.Add(attach);
+
+            Smtp.Send(Message);//отправка
+        }
+
+
+
+        public void Seach_Error(int i)
+        {
+            string HostName = Environment.MachineName;
+            if ((i == 1)&&(flag_cbank==false))
+            {
+                SendEmail("Пропал процесс CBMAIN.EX на сервере " + HostName + "! Проверьте сервис!!!",0);
+                flag_cbank = true;
+            }
+            if ((i == 2)&&(flag_rts==false))
+            {
+                SendEmail("Пропал процесс RTS.EXE на сервере " + HostName + "! Проверьте сервис!!!",0);
+                flag_rts = true;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+   
+            
+      //      procs = Process.GetProcessesByName("cbmain1.ex"); if (procs.Length == 0) Seach_Error(1);
+
+     //       procs = Process.GetProcessesByName("rts.exe"); if (procs.Length == 0) Seach_Error(2);
+
+            
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] procs1;
+            procs1 = Process.GetProcessesByName("cbmain.ex");
+            if (procs1.Length == 0)
+            {
+                Seach_Error(1);
+            }
+            else if (flag_cbank == true)
+            {
+                flag_cbank = false;
+                string HostName = Environment.MachineName;
+                SendEmail("Процесс CBANK.EXE на сервере " + HostName + " восстановлен!", 1);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Монитор сервиса Cbmain.ex")
+            {
+                timer1.Interval = Convert.ToInt16(numericUpDown1.Value) * 1000 * 60;
+                timer1.Enabled = true;
+                button1.Text = "Остановить Монитор сервиса Cbmain.ex";
+            }
+            else
+            {
+                timer1.Enabled = false;
+                button1.Text = "Монитор сервиса Cbmain.ex";
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Process[] procs2;
+            procs2 = Process.GetProcessesByName("rts");
+            if (procs2.Length == 0)
+            {
+                Seach_Error(2);
+            }
+            else if (flag_rts == true)
+            {
+                flag_rts = false;
+                string HostName = Environment.MachineName;
+                SendEmail("Процесс RTS.EXE на сервере " + HostName + " восстановлен!", 1);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.Text == "Монитор сервиса Rts.exe")
+            {
+                timer2.Interval = Convert.ToInt16(numericUpDown1.Value) * 1000 * 60;
+                timer2.Enabled = true;
+                button2.Text = "Остановить Монитор сервиса Rts.exe";
+            }
+            else
+            {
+                timer2.Enabled = false;
+                button2.Text = "Монитор сервиса Rts.exe";
+            }
+        }
+
+
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+                {
+                    this.ShowInTaskbar=false;
+                    notifyIcon1.Visible=true;
+                }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.ShowInTaskbar = true;
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IntPtr iHandle, iHandle2, iHandle3;
+                int i = 0;
+            const int nChars = 256;
+            StringBuilder Buff = new StringBuilder(nChars);
+            StringBuilder Buff2 = new StringBuilder(nChars);
+            iHandle = FindWindow(null, "Текущие сессии");
+            iHandle2 = FindWindowEx(iHandle, IntPtr.Zero, "TPanel", null);
+             i = GetWindowText(iHandle, Buff, nChars);
+            do {
+            //    iHandle2=GetWindow(iHandle, 7);
+
+                iHandle3 = FindWindowEx(iHandle2, IntPtr.Zero, "TLabel", null);
+                i = GetClassName(iHandle3,Buff2, nChars);
+                i = GetWindowText(iHandle3, Buff, nChars);
+                MessageBox.Show(Buff2.ToString() + "  " + Buff.ToString());
+
+            }
+            while (Buff.ToString()!="6");
+
+
+       //      iHandle2=FindWindowEx(iHandle,null,"Button",0);
+
+        //     int i = GetWindowText(iHandle2, Buff, nChars);
+        //    i = GetClassName(iHandle,"Button", nChars);
+           //  i = GetWindowText(iHandle, Buff, nChars);
+            
+            MessageBox.Show(Convert.ToString(iHandle));
+
+        //    HWND Wnd;
+        //    Wnd = FindWindow(null, "SQL Navigator");
+
+        }
+
+
+
+    }
+}
